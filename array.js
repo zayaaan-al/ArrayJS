136b19d59bfa81e42e682d608daa7f53422a3c7d
AL ZAYAN P <alzayanpoothakkal1156@gmail.com>
2025-08-06 05:43:38 +0530

    Initial commit

---
+++ array.js
@@ -0,0 +1,67 @@
+// ARRAY DESTRUCTURING
+
+// let arr=[1,2,3,5];
+// let [a,b,c,d]=arr;
+// console.log(a);
+// console.log(d);
+
+// SKIPPING ELEMENTS BY COMA
+
+// let arr=[1,3,5,6];
+// let [a, , b]=arr;
+// console.log(b);
+
+// rest operator
+
+// let arr=[2,4,5,6,7];
+// let [first,second,...rest]=arr;
+// console.log(rest);
+
+// CONCATS
+
+// let arr1=[1,3];
+// let arr2=[3,4,5];
+//  console.log(arr1.concat(arr2));
+
+
+
+// foreachloop
+
+// let arr=[2,3,4,5];
+// arr.forEach(num => console.log(num*2));
+
+// Map()
+
+// let arr=[1,2,3,4];
+// let squared=arr.map(num=> num*num);
+// console.log(squared);
+
+// filter()
+
+// let arr=[1,3,4,5];
+// let filtered=arr.filter(num=>num<5);
+// console.log(filtered);
+
+// find()
+
+// let arr=[1,3,4,7];
+// let find=arr.find(num=>num>2);
+// console.log(find);
+
+// sort()
+
+// let arr=[1,5,4,2,8,5,0];
+// arr.sort();
+// console.log(arr);
+
+// reverse()
+
+// let arr= [1,4,3,8,9,0,6];
+// arr.reverse();
+// console.log(arr);
+
+
+
+
+
+
